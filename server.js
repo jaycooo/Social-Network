efc184e486bc7fa81816b7a725977057790c1776
jaycooo <j.coronado@live.com>
2022-10-16 13:36:16 -0400

    Updating Server File

---
+++ server.js
@@ -0,0 +1,25 @@
+// Require express and mongoose
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use (express.static('public'));
+
+app.use(require('./routes'));
+
+//connect mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Social-Network', {
+    useFindAndModify: false,
+    useNewUrlParser: true
+    useUnifiedTopology: true
+});
+
+// Log mongoose queries
+mongoose.set('debug',true);
+
+app.listen(PORT, () => console.log(' connected on localhost:${PORT}******'));
