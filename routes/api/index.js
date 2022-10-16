0522df1dbcb62b9c24f1a3982d7f6a582501aa6f
jaycooo <j.coronado@live.com>
2022-10-16 13:44:55 -0400

    index js file update

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,15 @@
-// require Users and Thought Models
-const Thoughts =require('./Thoughts');
-const Users = require('./Users');
+// Require express router
+const router = require('express').Router();
 
-//Export Modules: Users and Thoughts
-module.exports = {Users, Thoughts };
+//Import all of the API routes
+const apiRoutes = require('./api');
+
+// add prefix of '/api' to all of the api routes
+router.use ('/api', apiRoutes);
+
+//404 Status error message
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error....</h1>');
+});
+
+module.exports = router;
